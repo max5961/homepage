5090cab068eef1a003a3c2df0268109bd19334e1
max5961 <mmorrissey5961@gmail.com>
2024-02-10 22:33:39 -0500

    Generate rough layout of bookmarks component

--- src/components/Bookmarks.tsx
+++ src/components/Bookmarks.tsx
@@ -1,5 +1,36 @@
 import React from "react";
 
 export default function Bookmarks(): React.ReactElement {
-    return <div className="bookmarks-container"></div>;
+    return (
+        <div className="bookmarks-container">
+            <TabGroupRow />
+            <TabGroupRow />
+            <TabGroupRow />
+        </div>
+    );
+}
+
+function TabGroupRow(): React.ReactNode {
+    return (
+        <div className="tab-group-row">
+            <TabGroup title={"tab0"} />
+            <TabGroup title={"tab1"} />
+            <TabGroup title={"tab2"} />
+        </div>
+    );
+}
+
+function TabGroup({ title }: { title: string }): React.ReactElement {
+    return (
+        <div className="tab-group-wrapper">
+            <div className="tab-group">
+                <div className="title">{title}</div>
+                <div className="content">
+                    <div className="content-stuff"></div>
+                    <div className="content-stuff"></div>
+                    <div className="content-stuff"></div>
+                </div>
+            </div>
+        </div>
+    );
 }
